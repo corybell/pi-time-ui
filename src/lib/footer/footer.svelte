67f5ff2fca931bf6dfcd47c1b9fce8baf8a75baf
Cory Bell <corybell21@gmail.com>
2021-08-14 11:58:33 -0500

    moving stuff

---
+++ src/lib/footer/footer.svelte
@@ -0,0 +1,21 @@
+
+<footer>
+  <!-- nothing here -->
+</footer>
+
+<style>
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 40px;
+  }
+
+  @media (min-width: 480px) {
+    footer {
+      padding: 40px 0;
+    }
+  }
+</style>
+  
