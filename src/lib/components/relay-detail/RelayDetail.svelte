96fdca6b067589297ad7594dbe6257b6f6a849df
Cory Bell <corybell21@gmail.com>
2021-08-21 22:22:24 -0500

    drawer

--- src/lib/components/relay-detail/RelayDetail.svelte
+++ src/lib/components/relay-detail/RelayDetail.svelte
@@ -1,16 +1,17 @@
 <script>
-  import { fly, fade } from 'svelte/transition'
+  import Drawer from "$lib/components/drawer/Drawer.svelte"
   import { relayDetailId, relays } from "$lib/store/store"
 
-  const flyInOut = { x: -500, y: 0, duration: 500 }
   let _relay
-
+  let isOpen = false
+  
   relayDetailId.subscribe(id => {
     if (id) {
       _relay = $relays.find(r => r.id === id)
     } else {
       _relay = undefined
     }
+    isOpen = Boolean(_relay)
   })
 
   function handleCancel() {
@@ -23,78 +24,12 @@
 </script>
 
 <style>
-  .relay-detail {
-    z-index: 1010;
-    background-color: var(--color-white);
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 100%;
-    width: 100%;
-    max-width: 500px;
-  }
-  .relay-detail-content {
-    padding: 1rem;
-  }
-  .overlay {
-    z-index: 1000;
-    background-color: rgba(0,0,0,0.5);
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    width: 100%;
-    height: 100%;
-  }
-  .overlay:hover {
-    cursor: pointer;
-  }
-  .btn-save {
-    color: #fff;
-    background-color: var(--color-primary);
-  }  
-  .btn-cancel {
-    color: var(--color-primary);
-    background-color: var(--color-white);
-    border: 2px solid var(--color-primary);
-  }
-  .btn-container {
-    display: flex;
-    justify-content: space-between;
-  }
-  @media (min-width: 480px) {
-    .relay-detail {
-      width: 70%;
-      padding: 1rem;
-    }
-  }
-  @media (min-width: 767px) {
-    .relay-detail {
-      width: 60%;
-      padding: 2rem;
-    }
-  }
+  
 </style>
 
-{#if _relay}
-  <div
-    in:fly="{flyInOut}" 
-    out:fly="{flyInOut}" 
-    class="relay-detail"
-  >
-    <div class="relay-detail-content">
-      <h2>{_relay.id}</h2>
-      <div>{_relay.name}</div>
-      <div>{_relay.timer?.hr}</div>
-      <div>{_relay.timer?.min}</div>
-    </div>
-    <div class="btn-container">
-      <button class="btn-cancel" on:click={handleCancel}>Cancel</button>
-      <button class="btn-save" on:click={handleSave}>Save</button>
-    </div>
-  </div>
-  <div class="overlay" in:fade out:fade on:click={handleCancel} />
-{/if}
+<Drawer bind:isOpen={isOpen} handleCancel={handleCancel} handleSave={handleSave}>
+  <h2>{_relay.id}</h2>
+  <div>{_relay.name}</div>
+  <div>{_relay.timer?.hr}</div>
+  <div>{_relay.timer?.min}</div>
+</Drawer>
