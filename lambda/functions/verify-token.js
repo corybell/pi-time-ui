7df319235e3de618435389a585abcce1bcdae0f5
Cory Bell <corybell21@gmail.com>
2021-12-19 20:09:50 -0600

    logging

--- lambda/functions/verify-token.js
+++ lambda/functions/verify-token.js
@@ -3,14 +3,18 @@ const { errorResponse, successResponse } = require('../response-helper')
 
 exports.handler = async (event) => {
   if (!event.headers || !event.headers.Authorization) {
-    return errorResponse(401, 'Unauthorized')
+    return errorResponse(401, 'Missing headers')
+  }
+  
+  if (!event.headers.Authorization) {
+    return errorResponse(401, 'Missing auth header')
   }
 
   const token = event.headers.Authorization.replace('Bearer ')
 
   const result = jwt.verify(token)
   if (!result) {
-    return errorResponse(401, 'Unauthorized')
+    return errorResponse(401, `Result ${result}`)
   }
 
   return successResponse({ })
