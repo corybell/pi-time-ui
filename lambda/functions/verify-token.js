8a0901ca67224a0e4ce772363363beb294e801d6
Cory Bell <corybell21@gmail.com>
2021-12-20 20:53:38 -0600

    log function headers

--- lambda/functions/verify-token.js
+++ lambda/functions/verify-token.js
@@ -2,20 +2,21 @@ const jwt = require('../jwt-helper')
 const { errorResponse, successResponse } = require('../response-helper')
 
 exports.handler = async (event) => {
-  if (!event.queryStringParameters || !event.queryStringParameters.token) {
-    return errorResponse(401, 'Missing qs')
-  }
+  console.log(event.headers)
+  // if (!event.queryStringParameters || !event.queryStringParameters.token) {
+  //   return errorResponse(401, 'Missing qs')
+  // }
   
-  if (!event.queryStringParameters.token) {
-    return errorResponse(401, 'Missing header')
-  }
+  // if (!event.queryStringParameters.token) {
+  //   return errorResponse(401, 'Missing header')
+  // }
 
-  const { token } = event.queryStringParameters
+  // const { token } = event.queryStringParameters
 
-  const result = jwt.verify(token)
-  if (!result) {
-    return errorResponse(401, `Result ${result}`)
-  }
+  // const result = jwt.verify(token)
+  // if (!result) {
+  //   return errorResponse(401, `Result ${result}`)
+  // }
 
   return successResponse({ })
 }
