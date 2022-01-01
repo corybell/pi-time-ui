6b193c56e07ff1073be5a1fa3c63c204e34cc3c5
Cory Bell <corybell21@gmail.com>
2021-12-31 21:42:10 -0600

    get api keys from env

--- lambda/functions/get-token.js
+++ lambda/functions/get-token.js
@@ -1,6 +1,9 @@
 const jwt = require('../jwt-helper')
 const { errorResponse, successResponse } = require('../response-helper')
 
+const { API_KEYS } = process.env
+const parsedApiKeys = JSON.parse(API_KEYS)
+
 exports.handler = async (event) => {
   if (event.httpMethod !== "POST") {
     return errorResponse(405, 'Method Not Allowed')
@@ -12,7 +15,7 @@ exports.handler = async (event) => {
     return errorResponse(400, 'Bad Request')
   }
   
-  if (params.password !== 'banana') {
+  if (!parsedApiKeys.includes(params.password)) {
     return errorResponse(400, 'Invalid password')
   }
 
